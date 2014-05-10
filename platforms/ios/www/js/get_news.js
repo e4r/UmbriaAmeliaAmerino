5c688007c39337a82df6ecee814d8dea22853a68
e4r <leo.mmcm@gmail.com>
2014-05-10 18:30:57 +0200

    a

--- platforms/ios/www/js/get_news.js
+++ platforms/ios/www/js/get_news.js
@@ -1,3 +1,36 @@
-$.get('http://mattorreguerrini.it/api', function(responseText) {
-    alert(responseText);
-});
+function loadXMLDoc()
+{
+    
+    a = document.getElementById("news-container");
+    
+    var xmlhttp;
+    if (window.XMLHttpRequest)
+    {// code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp=new XMLHttpRequest();
+    }
+    else
+    {// code for IE6, IE5
+        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange=function()
+    {
+        if (xmlhttp.readyState==4 && xmlhttp.status==200)
+        {
+            
+           a.innerHTML=xmlhttp.responseText;
+        }
+    }
+    xmlhttp.open("GET","http://www.mattorreguerrini.it/api",true);
+    xmlhttp.send();
+}
+
+loadXMLDoc();
+
+window.onload = function() {
+    init();
+    $(document).ready(function(){
+                      
+                      $("#news-container").animate({bottom:'250px'});
+                      
+                      });
+};
